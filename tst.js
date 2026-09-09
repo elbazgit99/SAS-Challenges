6de4472ca6a748ade5083d36482dae3c2a15bed9
elbazgit99 <helbaz124@gmail.com>
2026-09-09 22:57:39 +0100

    testing algos

--- tst.js
+++ tst.js
@@ -9,6 +9,59 @@
 // console.log(isInStock());
 
 
+// couhnt string without .length
+
+//for of
+
+function forOfLength(str){
+  let count = 0
+  for(let string of str){
+    count++
+  }
+  console.log(count);
+  
+}
+forOfLength("hamza")
+
+//for
+function forLength(str) {
+  count = 0
+  for(let i = 0;str[i] !== undefined; i++){
+    count++
+  }
+  console.log(count);
+  
+}
+forLength("hamza")
 
 
+// while
+function whileLength(str) {
+  let count = 0
+  let i = 0
+  while(str[i] !== undefined) {
+    count++
+    i++
+    
+  }
+  console.log(count);
+}
+whileLength("hamza")
 
+
+// do while
+
+function doWhileLength(str) {
+  let count = 0
+  let i = 0
+  do{
+    count++
+    i++
+    
+  }while(str[i] !== undefined) {
+
+    console.log(count);
+  }
+  
+}
+doWhileLength("hamza")
