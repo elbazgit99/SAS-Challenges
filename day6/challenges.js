24058e939fe5c33ebf3f69f28589127f0ad8371f
elbazgit99 <helbaz124@gmail.com>
2026-09-14 19:10:10 +0100

    Search & Sort Algorithms

--- day6/challenges.js
+++ day6/challenges.js
@@ -1,7 +1,6 @@
 /*Day 6: Search & Sort Algorithms
 Level 1 
 
-Linear Search Implementation:
 */
 
 console.log(`1.Linear Search Implementation: `+`-`.repeat(30)) //NOTE : ignore this line
@@ -37,9 +36,19 @@ console.log(findMin([5, 2, 3,1,7]))
  
 console.log(`3. Count Occurrences Search: `+`-`.repeat(30)) //NOTE : ignore this line
 
-function countAppears() {
-
+function countAppears(array, target) {
+    let count = 0
+        for(let i = 0; i < array.length; i++){
+        if(array[i] === target){
+        count +=  1
+            
+        }
+    }   
+    console.log(count);
+    
 }
+countAppears([1,1,5,2,5,5,4,5,6],5)
+
 
 // level 2
 
@@ -61,3 +70,31 @@ function BubbleSort(arr) {
     
 }
 BubbleSort([5,7,6,9,8])
+
+
+console.log(`2. Selection Sort `+`-`.repeat(30)) //NOTE : ignore this line
+
+function SelectionSort() {
+
+}
+
+
+
+console.log(`3. Sort Strings `+`-`.repeat(30)) //NOTE : ignore this line
+
+function SortString(arr){
+    let swap
+    for( let i = 0; i < arr.length; i++ ){
+        for( let j = 0; j < arr.length -1; j++){
+            if( arr[j].toLowerCase() > arr[j + 1].toLowerCase() ){
+                swap = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = swap
+            }
+        }
+    }
+    console.log( arr);
+    
+}
+SortString(["d","C","A","b","y"])
+
